ae64505f6a21c9bed06b9e76e4d62971e505a00d
camolinasim <cmutep@gmail.com>
2020-07-19 15:29:35 -0600

    better dynamic content

--- second_project_incremental/react-complete-guide/src/App.js
+++ second_project_incremental/react-complete-guide/src/App.js
@@ -6,11 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I am a React app and I am awesome :')</h1>
-        <h1>This is really working!</h1>
-        <Person/>
-        <Person/>
-        <Person/>
+        <h1>I am a React app</h1>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My hobbies: racing </Person>
+        <Person name="Sophie" age="26"/> 
       </div>
     );
     
--- second_project_incremental/react-complete-guide/src/Person/Person.js
+++ second_project_incremental/react-complete-guide/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
-const person = () => {
-    return <p>I'm a person and I am {Math.floor(Math.random() * 30)} years old!!</p>
+//we can output dynamic content as part of our JSX content
+const person = (props) => {
+    return <p>I'm {props.name} person and I am {props.age} years old!</p>
 }
 
 export default person;
