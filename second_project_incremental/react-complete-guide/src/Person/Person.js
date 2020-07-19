92d679aaa80112163e6c45148753a5075e165f3d
camolinasim <cmutep@gmail.com>
2020-07-19 15:38:23 -0600

    learned to use children as dynamic content

--- second_project_incremental/react-complete-guide/src/Person/Person.js
+++ second_project_incremental/react-complete-guide/src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from 'react';
 //we can output dynamic content as part of our JSX content
 const person = (props) => {
-    return <p>I'm {props.name} person and I am {props.age} years old!</p>
+    return (
+    <div>
+        <p>I'm {props.name} person and I am {props.age} years old!</p>
+    <p>{props.children}</p> {/* children refers to any element within the opening and closing tag of our component*/}
+
+
+    </div>
+        
+        )
 }
 
 export default person;
