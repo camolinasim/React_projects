e7441d0d43f779008c121cb2c28a0ffff5e0fd07
camolinasim <cmutep@gmail.com>
2020-07-19 15:09:25 -0600

    learning to use components

--- second_project_incremental/react-complete-guide/src/App.js
+++ second_project_incremental/react-complete-guide/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>I am a React app and I am awesome :')</h1>
-
+        <h1>This is really working!</h1>
+        <Person/>
       </div>
     );
-
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null ,'Hi, I am a React App!!') );
+    
   }
 }
 
