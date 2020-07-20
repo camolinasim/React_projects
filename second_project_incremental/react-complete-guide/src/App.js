d4df4ba640890b8680a3eeaa2453edcfd86d9fd2
camolinasim <cmutep@gmail.com>
2020-07-19 18:16:03 -0600

    learned about states

--- second_project_incremental/react-complete-guide/src/App.js
+++ second_project_incremental/react-complete-guide/src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'cris', age: 24},
+      {name: 'crost', age: 22},
+      {name: 'crust', age: 23}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>I am a React app</h1>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My hobbies: racing </Person>
-        <Person name="Sophie" age="26"/> 
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/> 
       </div>
     );
     
