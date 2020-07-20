45a6c562362361e8cd06cde4f1b65b065fea72ff
camolinasim <cmutep@gmail.com>
2020-07-20 17:44:52 -0600

    Revert "tried linting and then deactivated it because I went crazy"
    This reverts commit 3ab950ed5556581acc0906830756fb6476df7a58.

--- second_project_incremental/react-complete-guide/src/App.js
+++ second_project_incremental/react-complete-guide/src/App.js
@@ -5,53 +5,48 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 },
+      {name: 'cris', age: 24},
+      {name: 'crost', age: 22},
+      {name: 'crust', age: 23}
     ],
-    otherState: 'some other value',
-  };
+    otherState: 'some other value'
+  }
 
-  switchNameHandler = (newName) => {
-    // console.log('Button was clicked!')
-    //DONT DO THIS: this.state.persons[0].name = 'potato'
-    this.setState({
-      persons: [
-        { name: newName, age: 1 },
-        { name: 'B', age: 2 },
-        { name: 'C', age: 3 },
-      ],
-    });
-  }; //handler means this is a method you're not actively calling
+  switchNameHandler = () => {
+   // console.log('Button was clicked!')
+   //DONT DO THIS: this.state.persons[0].name = 'potato'
+   this.setState({
+    persons: [
+      {name: 'A', age: 1},
+      {name: 'B', age: 2},
+      {name: 'C', age: 3}
+    ]
+   })
+  } //handler means this is a method you're not actively calling
+
 
   render() {
     return (
       <div className="App">
         <h1>I am a React app</h1>
-        <button onClick={this.switchNameHandler.bind(this, 'button')}>
-          Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this, 'first')}
-        />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler}/>
+        
+        <Person 
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler}>My hobbies: racing </Person>
 
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'second')}
-        >
-          My hobbies: racing{' '}
-        </Person>
+        <Person name={this.state.persons[2].name}
+        age={this.state.persons[2].age}
+        click={this.switchNameHandler}/> 
 
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler.bind(this, 'third')}
-        />
       </div>
     );
+    
   }
 }
 
