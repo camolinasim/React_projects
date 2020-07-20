3ab950ed5556581acc0906830756fb6476df7a58
camolinasim <cmutep@gmail.com>
2020-07-19 22:01:47 -0600

    tried linting and then deactivated it because I went crazy

--- second_project_incremental/react-complete-guide/src/App.js
+++ second_project_incremental/react-complete-guide/src/App.js
@@ -5,48 +5,53 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'cris', age: 24},
-      {name: 'crost', age: 22},
-      {name: 'crust', age: 23}
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
     ],
-    otherState: 'some other value'
-  }
+    otherState: 'some other value',
+  };
 
-  switchNameHandler = () => {
-   // console.log('Button was clicked!')
-   //DONT DO THIS: this.state.persons[0].name = 'potato'
-   this.setState({
-    persons: [
-      {name: 'A', age: 1},
-      {name: 'B', age: 2},
-      {name: 'C', age: 3}
-    ]
-   })
-  } //handler means this is a method you're not actively calling
-
+  switchNameHandler = (newName) => {
+    // console.log('Button was clicked!')
+    //DONT DO THIS: this.state.persons[0].name = 'potato'
+    this.setState({
+      persons: [
+        { name: newName, age: 1 },
+        { name: 'B', age: 2 },
+        { name: 'C', age: 3 },
+      ],
+    });
+  }; //handler means this is a method you're not actively calling
 
   render() {
     return (
       <div className="App">
         <h1>I am a React app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person 
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age}
-        click={this.switchNameHandler}/>
-        
-        <Person 
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler}>My hobbies: racing </Person>
+        <button onClick={this.switchNameHandler.bind(this, 'button')}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'first')}
+        />
 
-        <Person name={this.state.persons[2].name}
-        age={this.state.persons[2].age}
-        click={this.switchNameHandler}/> 
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'second')}
+        >
+          My hobbies: racing{' '}
+        </Person>
 
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, 'third')}
+        />
       </div>
     );
-    
   }
 }
 
--- second_project_incremental/react-complete-guide/src/Person/Person.js
+++ second_project_incremental/react-complete-guide/src/Person/Person.js
@@ -1,15 +1,15 @@
 import React from 'react';
 //we can output dynamic content as part of our JSX content
 const person = (props) => {
-    return (
+  return (
     <div>
-        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-        <p onClick={props.click}>{props.children}</p> {/* children refers to any element within the opening and closing tag of our component*/}
-
-
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p onClick={props.click}>{props.children}</p>{' '}
+      {/* children refers to any element within the opening and closing tag of our component*/}
     </div>
-        
-        )
-}
+  );
+};
 
 export default person;
