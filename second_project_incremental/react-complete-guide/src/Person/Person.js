b59377e71ba3f3a52ca371cce07bfedf8d9861b5
camolinasim <cmutep@gmail.com>
2020-07-21 21:22:02 -0600

    learned about two way bindings

--- second_project_incremental/react-complete-guide/src/Person/Person.js
+++ second_project_incremental/react-complete-guide/src/Person/Person.js
@@ -1,15 +1,18 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+import React from "react";
 //we can output dynamic content as part of our JSX content
 const person = (props) => {
-    return (
+  return (
     <div>
-        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-        <p onClick={props.click}>{props.children}</p> {/* children refers to any element within the opening and closing tag of our component*/}
-
-
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p onClick={props.click}>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+      {/* children refers to any element within the opening and closing tag of our component*/}
     </div>
-        
-        )
-}
+  );
+};
 
 export default person;
