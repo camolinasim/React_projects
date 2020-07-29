2b3fd2541589aa32eb49e460009ca91473a11974
camolinasim <cmutep@gmail.com>
2020-07-28 22:18:43 -0600

    better

--- third_project/src/App.js
+++ third_project/src/App.js
@@ -4,12 +4,22 @@ import UserInput from "./UserInput/UserInput";
 import UserOutput from "./UserOutput/UserOutput";
 
 class App extends Component {
+  state = {
+    userOutputs: [{ name: "shen_op" }, { name: "gnar_op" }],
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      userOutputs: [{ name: "teemo" }, { name: "diana" }],
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <UserInput />
-        <UserOutput name="shen" />
-        <UserOutput name="ornn" />
+        <UserInput change={this.nameChangedHandler} />
+        <UserOutput name={this.state.userOutputs[0].name} />
+        <UserOutput name={this.state.userOutputs[1].name} />
       </div>
     );
   }
--- third_project/src/UserInput/UserInput.js
+++ third_project/src/UserInput/UserInput.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const UserInput = () => {
-  return <input type="text"></input>;
+const UserInput = (props) => {
+  return <input type="text" onChange={props.change}></input>;
 };
 
 export default UserInput;
